49b92a60f7beea5e7fc06c64702e1ec4867b1937
matus.benko <matus.benko@gmail.com>
2020-12-10 18:59:35 +0100

    make IR beam work on small distance

--- wifi-controller/WifiManager/WifiManager.ino
+++ wifi-controller/WifiManager/WifiManager.ino
@@ -1,83 +1,113 @@
-//#include <WiFiManager.h>
-//#include <WiFi.h>
+#include <WiFiManager.h>
+#include <WiFi.h>
 #include <IRsend.h>
 #include <IRrecv.h>
 #include <IRremoteESP8266.h>
-#include <IRutils.h>
-
-#define IRR 2 //G2
-#define IRS 4 //G4
 
 const uint32_t kBaudRate = 115200;
-const uint16_t kRecvPin = 14;
-const uint16_t kIrLedPin = 2;
-const uint16_t kCaptureBufferSize = 1024;
-const uint16_t kTimeout = 50;
-const uint16_t kFrequency = 38000;
+const uint16_t kReceiverPin = 14;
+const uint16_t kTransmitterPin = 4;
 
-IRsend irsend(kIrLedPin);
-//IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, false);
+IRrecv irrecv(kReceiverPin);
 decode_results results;
 
-//int port = 3000;
-//IPAddress server(192,168,178,25);
-//WiFiClient client;
+int port = 3000;
+IPAddress server(192,168,178,25);
+WiFiClient client;
+
+unsigned long currentMillis;
+unsigned long timer;
+const unsigned long beamPeriod = 2000;
+const unsigned long receiverPeriod = 700;
+const unsigned long pausePeriod = 10000;
+bool beamDetected;
+bool isBeamUp;
+
+// this is temporary helper to detect the PIN on the board by sending repeating signal to it
+void debugPin(int testPin) {
+  pinMode(testPin,OUTPUT);
+  digitalWrite(testPin, HIGH);
+  delay(500);  
+  digitalWrite(testPin, LOW);
+  delay(500);    
+}
+
+void startWifiManager() {
+  WiFiManager wm;
+  wm.autoConnect("NodeMCU");
+  Serial.println("Connected");
+}
+
+void pingServer() {
+  if (client.connect(server, port)) {
+    Serial.println("Client connected to server");
+    client.println("POST /post HTTP/1.0");
+    client.println();
+  }  
+}
 
 void setup() {
-
-//  pinMode(kIrLedPin, OUTPUT);
-  pinMode(kRecvPin, INPUT);
-  irsend.enableIROut(38);
-  
   Serial.begin(kBaudRate);
   while(!Serial) {
     delay(50);
   }
-  Serial.println();
-  Serial.print("DumbIRRepeater is now running and waiting for IR input on Pin ");
-  Serial.println(kRecvPin);
-  Serial.print("and will retransmit it on Pin ");
-  Serial.println(kIrLedPin);
+
+
+  pinMode(kTransmitterPin, OUTPUT);
+  irrecv.enableIRIn();
   
-//  WiFiManager wm;
-//  wm.autoConnect("NodeMCU");
-//  Serial.println("Connected");
-//
-//  if (client.connect(server, port)) {
-//    Serial.println("Client connected to server");
-//    client.println("POST /post HTTP/1.0");
-//    client.println();
-//  }
-////
-//  pinMode(IRS, OUTPUT);
-//  pinMode(IRR, INPUT);
+  //startWifiManager();
+  //pingServer();
+  
+  Serial.println("Setup finished");
+}
+
+void startBeam() {
+  Serial.println("Start beam");
+  digitalWrite(kTransmitterPin, HIGH);
+  isBeamUp = true;
+}
+
+void stopBeam() {
+  Serial.println("Stop beam");
+  digitalWrite(kTransmitterPin, LOW);
+  isBeamUp = false;
+}
+
+bool detectBeam() {
+  bool isBeamDetected = false;  
+  if (irrecv.decode(&results)) {
+    isBeamDetected = true;
+    irrecv.resume();
+  }
+  return isBeamDetected;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  Serial.println("Idle...");
+  currentMillis = millis();
 
-//  int testPin = kIrLedPin;
-//  pinMode(testPin,OUTPUT);
-//  digitalWrite(testPin, HIGH);
-//  delay(500);  
-//  digitalWrite(testPin, LOW);
-//  delay(500);  
-
-
-//  irsend.space(0);
-//  delay(1);
-  irsend.mark(1000/(38*2));
-//
-//  
-//  digitalWrite(kIrLedPin, LOW);
-//  delay(1);
-//  digitalWrite(kIrLedPin, HIGH);
-//  
-
-   int recv = digitalRead(kRecvPin);
-   Serial.println(recv);
-   delay(50);
-
+  bool isFirstRun = timer == 0;
+  bool isPauseOver = (currentMillis - timer) >= (beamPeriod + pausePeriod);
   
+  if ((isFirstRun || isPauseOver) && !isBeamUp) {
+    startBeam();
+    beamDetected = false;
+    timer = currentMillis;
+  }
+
+  bool receiverStart = (currentMillis - timer) >= (beamPeriod - receiverPeriod) / 2;
+  bool receiverStop = (currentMillis - timer) >= ((beamPeriod - receiverPeriod) / 2) + receiverPeriod;
+  if (receiverStart && !receiverStop) {
+    beamDetected = beamDetected || detectBeam();    
+  }
+
+  bool beamStop = ((currentMillis - timer) >= beamPeriod);
+  if (beamStop && isBeamUp) {
+    stopBeam();
+  
+    Serial.print("Beam detected = ");
+    Serial.println(beamDetected);
+  }
+  
+  delay(20);
 }
